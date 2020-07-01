8bce38a2ad4170bd82b77db94f016e5d8f7965fd
Cassia Melvin <cassiamelvin@gmail.com>
2020-07-01 15:08:55 -0400

    Initial Commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'b69c7a7a6c8969ff3531';
+    this.client_secret = 'eafc1ca03d2b708ef913e823687bd7b94dfc29b2';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
